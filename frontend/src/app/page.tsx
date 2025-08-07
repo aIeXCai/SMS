c13e6b5bc4e95449acf3e80789f36e14c1a0bf76
aIeXCai <23041833g@connect.polyu.hk>
2025-08-07 23:29:13 +0800

    Frontend Framework Built

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+// This is a demo of a preview
+// That's what users will see in the preview
+
+import { FullScreenSignup } from "@/components/ui/full-screen-signup";
+
+const SignupPage = () => {
+  return <FullScreenSignup />;
+};
+
+export default SignupPage;
